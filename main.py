c949094020503b2cb5c28dda629e3bc4e9d4555b
Lucas <lucastavasousa@gmail.com>
2021-08-02 20:03:03 -0300

    fazendo janela padrao

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+from PPlay import window
+
+
+janela = window.Window(400,300)
+
+while 1:
+    
+     
+    janela.set_background_color((0,0,0))  
+    
+    janela.update()
+
+    
---
+++ telas/JanelaPadrao.py
@@ -0,0 +1,14 @@
+import pygame
+class JanelaPadrao:
+
+    def __init__(self, janela):
+        self.janela = janela
+        self.pygame = pygame
+
+
+    def trataEvento(self):
+        for e in pygame.event.get():
+            self.evento(e)
+    
+    def evento(self):
+        pass
