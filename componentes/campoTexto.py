187aa28b2d642b959652a62e29030e6244bb7681
Lucas <lucastavasousa@gmail.com>
2021-08-03 10:21:03 -0300

    terminando campo de texto

--- componentes/campoTexto.py
+++ componentes/campoTexto.py
@@ -2,18 +2,20 @@ from typing import Text
 from  componentes.componenteGeral import ComponenteGeral
 import pygame
 class CampoTexto(ComponenteGeral):
-    def __init__(self, window, x=0, y=0, w=0, h=0):
+    def __init__(self, window, x=0, y=0, w=0, h=0,size = 24):
         super().__init__(window, x, y, w, h)
         self.retango_fora = pygame.Rect([self.x,self.y,self.width,self.height])
         self.retango_dentro = pygame.Rect([self.x,self.y,self.width,self.height])
         self.texto = ""
+        self.size = size
 
 
     def draw(self):
         super().draw()
         pygame.draw.rect(self.window.get_screen(), (255,255,255),self.retango_dentro)
         pygame.draw.rect(self.window.get_screen(), (0,0,0),self.retango_fora, 1)
-        self.window.draw_text(self.texto, self.x+5,self.y+self.height/2)
+        
+        self.window.draw_text(self.texto, self.x+5,self.y+self.height/4, size = self.size)
 
 
     def evento(self, e):
