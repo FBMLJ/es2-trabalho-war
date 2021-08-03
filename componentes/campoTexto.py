260e8e7637ce490d3da69e260d1b1839675e21d3
Lucas <lucastavasousa@gmail.com>
2021-08-03 10:27:05 -0300

    campo de texto quase finalizado

--- componentes/campoTexto.py
+++ componentes/campoTexto.py
@@ -2,11 +2,12 @@ from typing import Text
 from  componentes.componenteGeral import ComponenteGeral
 import pygame
 class CampoTexto(ComponenteGeral):
-    def __init__(self, window, x=0, y=0, w=0, h=0,size = 24):
+    def __init__(self, window, x=0, y=0, w=0, h=0,size = 24,tamanho_maximo=20):
         super().__init__(window, x, y, w, h)
         self.retango_fora = pygame.Rect([self.x,self.y,self.width,self.height])
         self.retango_dentro = pygame.Rect([self.x,self.y,self.width,self.height])
         self.texto = ""
+        self.tamanho_maximo = tamanho_maximo
         self.size = size
 
 
@@ -15,7 +16,10 @@ class CampoTexto(ComponenteGeral):
         pygame.draw.rect(self.window.get_screen(), (255,255,255),self.retango_dentro)
         pygame.draw.rect(self.window.get_screen(), (0,0,0),self.retango_fora, 1)
         
-        self.window.draw_text(self.texto, self.x+5,self.y+self.height/4, size = self.size)
+        self.window.draw_text(
+            # Essa parte pega somente as letras que irão ser exibidas na tela
+            self.texto[max(0,len(self.texto)-self.tamanho_maximo):], 
+        self.x+5,self.y+self.height/4, size = self.size, font_name ="FreeMono, Monospace")
 
 
     def evento(self, e):
