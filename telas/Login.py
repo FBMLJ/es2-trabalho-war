68b533df3b31298d161338f47365c9cbf3f1c903
Lucas <lucastavasousa@gmail.com>
2021-08-02 22:54:39 -0300

    começando fazer tela de login

---
+++ telas/Login.py
@@ -0,0 +1,8 @@
+from telas.JanelaPadrao import JanelaPadrao
+class Login(JanelaPadrao):
+    def __init__(self, janela):
+        super().__init__(janela)
+        
+
+    def draw(self):
+        return super().draw()
