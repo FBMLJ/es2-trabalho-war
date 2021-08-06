7b03351054b9c93b347548be44d8d74a9c4818c6
Andre Fernandes Goncalves <andrefg@id.uff.br>
2021-08-05 21:45:43 -0300

    Implementacao inicial do mapa, mais a vir

---
+++ telas/ControladorMapa.py
@@ -0,0 +1,22 @@
+
+
+class ControladorMapa:
+    dicionario_territorios = {
+        "Congo": 1,
+        "Sudao": 2,
+        "Egito": 3,
+        "Madagascar": 4,
+        "Argelia": 5,
+        "Africa do Sul": 6,
+        "California": 7,
+        "Vancouver": 8,
+        "Ottawa": 9,
+        "Labrador": 10,
+        "Alaska": 11,
+        "Mackenzie": 12,
+        "Groenlandia": 13,
+
+    }
+
+    def __init__(self):
+        pass
