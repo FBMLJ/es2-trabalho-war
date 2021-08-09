1e8482db4aa14e65afcb2194e3cdd5d7a371392f
André Fernandes Gonçalves <andrefg@id.uff.br>
2021-08-08 22:08:17 -0300

    tentando atualizar pasta jogo

---
+++ jogo/Continente.py
@@ -0,0 +1,14 @@
+import Territorio
+
+class Continente:
+
+    def __init__(self, nome, lista_territorios, tropas_bonus):
+        self.nome = nome
+        self.territorios = lista_territorios
+        self.bonus = tropas_bonus
+
+    def continente_conquistado(self, cor_jogador):
+        for territorio in self.territorios:
+            if territorio.cor_tropas != cor_jogador:
+                return False
+        return True
