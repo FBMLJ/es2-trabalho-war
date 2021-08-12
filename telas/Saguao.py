cb7fc46d08f9f1533b96d6b9462f430cb42c7f67
danielbougleux <danielbougleux@id.uff.br>
2021-08-12 17:45:20 -0300

    layout do saguao finalizado

--- telas/Saguao.py
+++ telas/Saguao.py
@@ -21,12 +21,52 @@ class Saguao:
         self.botoes = []
 
         self.barra_superior = GameImage("assets/imagem/saguao/barra_saguao.png")
-        self.barra_superior.set_position(self.fundo.x + 16, self.fundo.y + 10)
+        self.barra_superior.set_position(self.fundo.x + 12, self.fundo.y + 10)
 
-        sprite_x = Sprite("assets/imagem/busca_saguao/x_saguao.png")
+        sprite_x = Sprite("assets/imagem/saguao/x_saguao.png")
         botao_x = Botao(sprite_x, sprite_x, self.codigo_botoes["sair"])
-        botao_x.setposition(self.fundo.x + self.fundo.width - botao_x.width - 20, self.fundo.y + 15)
+        botao_x.setposition(self.fundo.x + self.fundo.width - botao_x.width - 16, self.fundo.y + 15)
         self.botoes.append(botao_x)
+
+        sprite_enviar_normal = Sprite("assets/imagem/saguao/botao_enviar.png")
+        sprite_enviar_destacado = Sprite("assets/imagem/saguao/botao_enviar_select.png")
+        botao_enviar = Botao(sprite_enviar_normal, sprite_enviar_destacado, self.codigo_botoes["enviar"])
+        botao_enviar.setposition(
+            self.fundo.x + self.fundo.width - botao_enviar.width - 40,
+            self.fundo.y + self.fundo.height - botao_enviar.height - 40
+        )
+        self.botoes.append(botao_enviar)
+
+        sprite_pronto_normal = Sprite("assets/imagem/saguao/botao_pronto.png")
+        sprite_pronto_destacado = Sprite("assets/imagem/saguao/botao_pronto_select.png")
+        botao_pronto = Botao(sprite_pronto_normal, sprite_pronto_destacado, self.codigo_botoes["pronto"])
+        botao_pronto.setposition(
+            self.fundo.x + 80,
+            self.fundo.y + self.fundo.height - botao_pronto.height - 40
+        )
+        self.botoes.append(botao_pronto)
+
+        self.campo_chat = CampoTexto(
+            janela,
+            GameImage("assets/imagem/saguao/digite_mensagem.png"),
+            self.fundo.x + self.fundo.width - 640,
+            self.fundo.y + self.fundo.height - botao_pronto.height - 36,
+            450,
+            50,
+            25
+        )
+
+        self.legenda_participantes = GameImage("assets/imagem/saguao/participantes.png")
+        self.legenda_participantes.set_position(
+            self.fundo.x + 50,
+            self.barra_superior.y + self.barra_superior.height + 10
+        )
+
+        self.legenda_chat = GameImage("assets/imagem/saguao/chat.png")
+        self.legenda_chat.set_position(
+            self.fundo.x + self.fundo.width - self.legenda_chat.width - 300,
+            self.barra_superior.y + self.barra_superior.height + 10
+        )
 
     def loop(self):
 
@@ -57,6 +97,7 @@ class Saguao:
     def trataEvento(self):
 
         for evento in pygame.event.get():
+            self.campo_chat.evento(evento)
             if evento.type == pygame.QUIT:
                 exit()
 
@@ -64,5 +105,8 @@ class Saguao:
 
         self.fundo.draw()
         self.barra_superior.draw()
+        self.campo_chat.draw()
+        self.legenda_participantes.draw()
+        self.legenda_chat.draw()
         for botao in self.botoes:
             botao.render()
