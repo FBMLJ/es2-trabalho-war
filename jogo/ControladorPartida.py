30117d2168ba487b7e580e858c28f0c57108232e
André Fernandes Gonçalves <andrefg@id.uff.br>
2021-09-14 15:02:47 -0300

    modificacoes em cartas e inicializao da partida
    mudancas em algumas responsabilidades em CardManager e ControladorPartida

--- jogo/ControladorPartida.py
+++ jogo/ControladorPartida.py
@@ -4,38 +4,63 @@ from PPlay.mouse import Mouse
 from jogo.ControladorMapa import ControladorMapa
 from jogo.MatchStarter import MatchStarter
 from jogo.Card import Card
+from jogo.Player import Player
+from jogo.CardManager import CardManager
+from jogo.ObjectiveVerifier import ObjectiveVerifier
+from jogo.TroopsManager import TroopsManager
 from componentes.hudTurno import hudTurno
 from constant import *
 
 class ControladorPartida:
     caminho_hud = "assets/imagem/hud/"
 
-    def __init__(self, janela:Window):
+    def __init__(self, janela:Window, jogadores:list):
         self.controlador_mapa = ControladorMapa(janela)
-        self.iniciador_de_partida = MatchStarter()
+        self.iniciador_partida = MatchStarter(jogadores)
+        self.gerenciador_cartas = CardManager()
+        self.gerenciador_objetivos = ObjectiveVerifier()
+        self.gerenciador_tropas = TroopsManager()
         self.hud_turno = hudTurno(janela)
         self.janela = janela
         self.mouse = Mouse()
-        self.todas_as_cartas = [
-            # Coringas
-            Card(None, "coringa_carta.png", None, True),
-            Card(None, "coringa_carta.png", None, True)
-        ]
+        self.jogadores = jogadores
+        self.jogador_vencedor = None
+        self.baralho = []
+        self.rodada = 1
 
     def loop(self):
         self.janela.clear()
-        while True:
-            self.controlador_mapa.selecionar_territorio(self.mouse)
-            self.render()
-            self.janela.update()
+        
+        #Inicializacao do jogo: distribuir os territorios e objetivos para todos os jogadores
+        self.baralho = self.gerenciador_cartas.inicia_cartas()
+        self.iniciador_partida.distribui_territorios(self.controlador_mapa.lista_territorios, self.jogadores)
+        for jogador in self.jogadores:
+            self.gerenciador_tropas.recebimento_inicial(jogador)  #Colocando um exercito em cada territorio do jogador
+        self.iniciador_partida.distribui_objetivos()
+
+        #game loop
+        while self.jogador_vencedor == None:  #Condicional para checar se algum objetivo foi alcancado
+            for jogador in self.jogadores:
+                self.jogador_vencedor = self.gerenciador_objetivos(self.jogadores, self.controlador_mapa.lista_territorios)
+                if self.jogador_vencedor:
+                    break
+                '''
+                Etapa 1 do turno: Distribuicao de exercitos
+                '''
+                
+                '''
+                Etapa 2 do turno: Combate
+                '''
+                if(self.rodada > 1): #Na primeira rodada nenhum jogador pode atacar
+                    break
+                '''
+                Etapa 3 do turno: Movimentacao de exercitos
+                '''
+                self.controlador_mapa.selecionar_territorio(self.mouse)
+                self.render()
+                self.janela.update()
+            
 
     def render(self):
         self.controlador_mapa.render()
         self.hud_turno.render()
-
-    def inicia_cartas(self) -> None:
-        for id_territorio in dicionario_territorios:
-            nome_territorio = dicionario_territorios[id_territorio]
-            imagem = nome_territorio + "_carta.png"
-            figura = dicionario_figura_territorio[id_territorio]
-            self.todas_as_cartas.append(Card(nome_territorio, imagem, figura, False))
